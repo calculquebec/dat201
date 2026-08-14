191b3a9867c82b16955a029944b19d7322607ac1
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2026-08-14 18:08:34 -0400

    Script preparer_air.py qui génère air_stations.csv

---
+++ data/preparer_air.py
@@ -0,0 +1,91 @@
+import numpy as np
+import os
+import pandas as pd
+import sys
+
+
+def charger_donnees(debug_level=0):
+    air_df_list = []
+
+    for periode in ['2023', '2024']:
+        fichier_csv = f'mesure-impact-projets-verdissement-{periode}.csv'
+        air_df = pd.read_csv(fichier_csv)
+
+        air_df['date_heure'] = pd.to_datetime(
+            air_df['date_heure'],
+            format='%m/%d/%y %H:%M:%S' if periode == '2024' else None
+        )
+
+        air_df_list.append(air_df)
+
+    for periode in ['janv-juin-2025', 'juil-dec-2025']:
+        fichier_csv = f'mesure-impact-projets-verdissement-{periode}.csv'
+        air_df = pd.read_csv(fichier_csv, sep=';', decimal=',')
+
+        air_df['date_heure'] = pd.to_datetime(
+            air_df['date_heure'],
+            format='%m/%d/%y %H:%M:%S %z',
+            utc=True
+        ).dt.tz_convert(None)
+
+        for nom_colonne in ['longitude', 'latitude']:
+            air_df[nom_colonne] = air_df[nom_colonne].astype('float')
+
+        air_df_list.append(air_df)
+
+    if debug_level >= 1:
+        for air_df in air_df_list:
+            print(air_df.dtypes)
+
+    return pd.concat(air_df_list).reset_index(drop=True)
+
+
+def enlever_colonnes(df, colonnes):
+    for nom_colonne in colonnes:
+        df.drop(columns=nom_colonne, inplace=True)
+
+
+def preparer_stations(air_df):
+    id_stations = air_df['id_station'].str.replace('-1$', '', regex=True)
+    id_stations = id_stations.str.split().str[1]  # '12345678:87654321'
+    id_stations = id_stations.str.split(':').str[0]  # '12345678'
+    id_stations = id_stations.astype('int')  # 12345678
+    air_df['id_station'] = id_stations
+
+    air_df['nom_station'] = air_df['nom_station'].str.replace('é', 'e')
+
+    masque_e67 = (
+        (air_df['id_station'] == 21317941) &
+        (air_df['nom_station'] == 'Espace_67')
+    )
+    air_df.loc[masque_e67, 'nom_station'] = 'Carref_Langelier'
+
+    masque_jde = (
+        (air_df['nom_station'] == 'Jean-Drapeau_Etang') &
+        (air_df['longitude'] == -73.556306)
+    )
+    air_df.loc[masque_jde, 'longitude'] = -73.532472
+    air_df.loc[masque_jde, 'latitude'] = 45.518028
+
+    stations_df = air_df[
+        ['id_station', 'nom_station', 'longitude', 'latitude']
+    ].drop_duplicates().sort_values('id_station')
+
+    stations_df.to_csv('air_stations.csv', index=False)
+    enlever_colonnes(air_df, stations_df.columns[1:])
+
+
+def preparer_air():
+    air_df = charger_donnees()
+
+    preparer_stations(air_df)
+
+
+def main():
+    os.chdir(sys.path[0])
+
+    preparer_air()
+
+
+if __name__ == '__main__':
+    main()
